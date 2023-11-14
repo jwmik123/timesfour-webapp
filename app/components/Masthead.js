692857ff69c5a92c19d84d8ef2cd5f32095b2e4b
Joël Mik <contact@joelmik.nl>
2023-11-14 13:47:27 +0100

    add: delay to experience objects in masthead

--- app/components/Masthead.js
+++ app/components/Masthead.js
@@ -1,14 +1,31 @@
 "use client";
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./Experience";
+import { useState, useCallback } from "react";
 import { gsap } from "gsap";
+import { Canvas } from "@react-three/fiber";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useState } from "react";
+
+import { Experience } from "./Experience";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Masthead() {
   const [hoveredItem, setHoveredItem] = useState("Development");
+
+  const debounce = (func, delay) => {
+    let inDebounce;
+    return function () {
+      const context = this;
+      const args = arguments;
+      clearTimeout(inDebounce);
+      inDebounce = setTimeout(() => func.apply(context, args), delay);
+    };
+  };
+
+  const setHoveredItemDebounced = useCallback(
+    debounce(setHoveredItem, 150),
+    []
+  );
+
   return (
     <div className="relative min-h-full mx-10">
       <span className="underline underline-offset-2">A creative studio.</span>
@@ -21,12 +38,12 @@ export default function Masthead() {
       </Canvas>
       <div
         style={{ userSelect: "none" }}
-        className={` absolute top-0 font-rift font-semibold leading-[14vw] text-[15vw] flex flex-col w-full py-5 justify-center h-full cursor-default`}
+        className={` absolute top-0 font-rift font-semibold leading-[14vw] text-[15vw] flex flex-col w-full py-5  h-full cursor-default`}
       >
         <div className="flex justify-between">
           <span>times</span>
           <span
-            onMouseEnter={() => setHoveredItem("Marketing")}
+            onMouseEnter={() => setHoveredItemDebounced("Marketing")}
             className="transition-all duration-300 opacity-20 hover:opacity-100 hover:text-yellow-400"
           >
             marketing
@@ -34,14 +51,14 @@ export default function Masthead() {
         </div>
         <div className="flex justify-between">
           <span
-            onMouseEnter={() => setHoveredItem("Design")}
+            onMouseEnter={() => setHoveredItemDebounced("Design")}
             className="transition-all duration-300 opacity-20 hover:opacity-100 hover:text-yellow-400"
           >
             design
           </span>
           <span>four</span>
           <span
-            onMouseEnter={() => setHoveredItem("Film")}
+            onMouseEnter={() => setHoveredItemDebounced("Film")}
             className="transition-all duration-300 opacity-20 hover:opacity-100 hover:text-yellow-400"
           >
             film
@@ -50,7 +67,7 @@ export default function Masthead() {
         <div className="flex justify-between">
           <span>x4</span>
           <span
-            onMouseEnter={() => setHoveredItem("Development")}
+            onMouseEnter={() => setHoveredItemDebounced("Development")}
             className="transition-all duration-300 opacity-20 hover:opacity-100 hover:text-yellow-400"
           >
             development
--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import VideoPlayer from "@/app/components/VideoPlayer";
 export default function Home() {
   useLayoutEffect(() => {
     const lenis = new Lenis({
-      duration: 1.4,
+      duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       direction: "vertical", // vertical, horizontal
       gestureDirection: "vertical", // vertical, horizontal, both
